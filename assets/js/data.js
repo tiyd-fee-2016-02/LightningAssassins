3d115a78213d9c7224b3cc4823913b4494ef4986
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-26 12:48:15 -0500

    testing local storage

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,9 +1,27 @@
+//--------LOCAL STORAGE STUFF--------//
+
 $(function (){
   'use strict';
 
+// TESTING LOCAL STORAGE //
+
+// create a variable with an object
+var hello = { 'one': 1, 'two': 2, 'three': 3 };
+
+// add to local storage and turn into a string
+localStorage.setItem('hello', JSON.stringify(hello));
+
+// get from local storage
+var printObject = localStorage.getItem('hello');
+
+console.log(printObject);
 
 
+// console.log('hello: ', JSON.parse(printObject));
 
+$.each( hello, function( key, value ) {
+  alert( key + ": " + value );
+});
 
 
 
