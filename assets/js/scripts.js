3e46fce32a05da0edaec0ed8497f766c6ca23013
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-16 14:19:41 -0400

    module and controller works

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,55 +1,25 @@
-//-------- PAGE REDIRECTS CODE -------- //
-
-$(function (){
-  'use strict';
-
-//LEFT ARROW ON NEW-APPT PAGE REDIRECTS TO MAIN PAGE
-$('#back-button').on('click', function (){
-  window.location = "main-page.html";
-});
-
-//LEFT ARROW ON EDIT-APPT PAGE REDIRECTS TO DETAIL PAGE
-$('#back-button-two').on('click', function (){
-  window.location = "detail.html";
-});
-
-//PLUS BUTTON ON DETAIL PAGE REDIRECTS TO MAIN PAGE
-$('#detail-return-main').on('click', function (){
-  window.location = "main-page.html";
-});
-
-//SAVE BUTTON ON NEW APPT PAGE REDIRECTS TO MAIN PAGE
-$('#save-new-appointment').on('click', function (){
-  window.location = "main-page.html";
-});
-
-//SAVE OR DELETE BUTTON ON EDIT APPT PAGE REDIRECTS TO DETAILS PAGE
-$('#save-edit-appointment').on('click', function (){
-  window.location = "detail.html";
-});
-
-//EDIT BUTTON ON DETAILS PAGE REDIRECTS TO EDIT-APPT PAGE
-$('#editAppointment').on('click', function (){
-  window.location = "edit-appt.html";
-});
-
-//PLUS BUTTON ON MAIN PAGE REDIRECTS TO NEW-APPT PAGE
-$('#addAppointmentButton').on('click', function (){
-  window.location = "new-appt.html";
-});
-
-//DELETE BUTTON ON EDIT APPT PAGE TO TRIGGER MODAL
-$('#delete-edit-appointment').click (function(){
-  $(".modalContainer").addClass("deleteModal");
-});
-
-$('.cancelDelete').click(function(){
-  $(".modalContainer").removeClass("deleteModal");
-});
-
-$('.confirmDelete').click(function(){
-  window.location = "main-page.html";
-});
 
 
-});
+// create the module and name it apptApp
+var apptApp = angular.module('apptApp', []);
+
+apptApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'THIS WORKS, I HOPE';
+  });
+
+// ;function(){
+//   angular.module('apptApp', ['ngRoute'], function($routeProvider){
+//     $routeProvider
+//         .when('/', {
+//           templateUrl: '/main-page.html'
+//         })
+//   });
+// }
+    //
+    // apptApp.config(function($routeProvider){
+    //   $routeProvider
+    //     .when('/', {
+    //       templateUrl: '/main-page.html'
+    //     })
+    // });
