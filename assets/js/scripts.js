b6945fe8033ad521699fa4fb29f5e251f2b4e7c9
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-16 15:19:19 -0400

    first partial and route complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,30 @@
 
 
 // create the module and name it apptApp
-var apptApp = angular.module('apptApp', []);
+var apptApp = angular.module('apptApp', ['ngRoute']);
+
+
+apptApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'main-page.html',
+            })
+          });
+
+//   ;(function(){
+//     angular.module('apptApp', ['ngRoute'], function($routeProvider){
+//     $routeProvider
+//         .when('/', {
+//           templateUrl: 'main-page.html'
+//         }) //closes when
+//     }); //closes function
+// }// closes IIFE
+
 
 apptApp.controller('mainController', function($scope) {
+    console.log("this works?")
     // create a message to display in our view
     $scope.message = 'THIS WORKS, I HOPE';
   });
-
-// ;function(){
-//   angular.module('apptApp', ['ngRoute'], function($routeProvider){
-//     $routeProvider
-//         .when('/', {
-//           templateUrl: '/main-page.html'
-//         })
-//   });
-// }
-    //
-    // apptApp.config(function($routeProvider){
-    //   $routeProvider
-    //     .when('/', {
-    //       templateUrl: '/main-page.html'
-    //     })
-    // });
